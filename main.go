1576de32f95c9854ac9e0ea3486e8cf28f7886a8
IhsanMaul56 <sansgrids5601@gmail.com>
2025-09-22 14:39:24 +0700

    multiple route

--- main.go
+++ main.go
@@ -7,21 +7,52 @@ package main
 //     fmt.Println("Aku Tampan!")
 // }
 
+// Source untuk route
+// import (
+//     "log"
+//     "net/http"
+// )
+
+// func home(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello dari Aku Tampan"))
+// }
+
+// func main() {
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/", home)
+
+// 	log.Print("Starting serve on: 4000")
+
+// 	err := http.ListenAndServe(":4000", mux)
+// 	log.Fatal(err)
+// }
+
+// Berbeda route
 import (
     "log"
     "net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello dari Aku Tampan"))
+    w.Write([]byte("Ini adalah HOME page"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Ini adalah view snippest"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Ini adalah create snippest"))
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+    mux := http.NewServeMux()
+    mux.HandleFunc("/{$}", home)
+    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting serve on: 4000")
+    log.Print("starting server on :4000")
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+    err := http.ListenAndServe(":4000", mux)
+    log.Fatal(err)
 }
