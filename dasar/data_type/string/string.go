b7e84fd8e18d60696b6a2c10bd8b4a2abb12574b
IhsanMaul56 <sansgrids5601@gmail.com>
2025-10-13 13:21:35 +0700

    string

---
+++ dasar/data_type/string/string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main () {
+	fmt.Println("ihsan");
+	fmt.Println("saya ihsan");
+
+	fmt.Println(len("ihsan"));
+	fmt.Println("saya ihsan"[0]);
+	fmt.Println("saya ihsan"[1]);
+}
